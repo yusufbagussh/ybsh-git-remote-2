7e4e071337d58117ca8ccb407df50db4bc0bc44e
yusufbagussh <yusuf.herlambang27@gmail.com>
2020-09-04 17:17:31 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,8 @@
 <body>
 	<h1>Hello World</h1>
 	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, aliquid, ut! Dolores fuga blanditiis obcaecati, fugiat at reiciendis iusto id debitis tenetur nam eos incidunt quam quo laudantium cupiditate, quod!</p>
+	<ul>
+		<li></li>
+	</ul>
 </body>
 </html>
