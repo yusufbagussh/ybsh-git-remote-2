24cbce7af34c6c872f68ffd46dcf4d8f86a6f888
yusufbagussh <yusuf.herlambang27@gmail.com>
2020-09-04 17:09:48 +0700

    Menambahkan judul dan paragraf

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Coba Git Remote 2</title>
+</head>
+<body>
+	<h1>Hello World</h1>
+	<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse, aliquid, ut! Dolores fuga blanditiis obcaecati, fugiat at reiciendis iusto id debitis tenetur nam eos incidunt quam quo laudantium cupiditate, quod!</p>
+</body>
+</html>
